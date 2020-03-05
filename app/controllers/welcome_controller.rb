32a48e5d81700c62b8b2918a83d9d4898156309c
Matt Brown <brownprobable@gmail.com>
2020-03-05 13:34:28 +1300

    added chart.js

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,11 @@ class WelcomeController < ApplicationController
     ads = Advertisement.order(created_at: :desc).where("user_id = ?", current_user[:id])
     @ads = ads.limit(6)
     # Totaling the value of ads running in the current month
-    @this_months_issues = Issue.where(pub_date: Time.now.beginning_of_month..Time.now.end_of_month)
-    #@month_total = ads.map{ |a| }
-
+    this_months_issues = Issue.where(pub_date: Time.now.beginning_of_month..Time.now.end_of_month)
+    total = 0
+    this_months_issues.each do |i|
+      i.advertisements.map{ |a| a.user_id == current_user.id ? total += a.price : nil }
+    end
+    @total = total
   end
 end
